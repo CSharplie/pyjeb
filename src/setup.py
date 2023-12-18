d92e35510907e66e32860d45128721099aed17ba
CSharplie <charlie.collier@outlook.fr>
2023-12-19 00:41:49 +0100

    Chnage version

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup (
     name = "pyjeb",
-    version = "0.1.6",
+    version = "0.1.7",
     description="A lightweight library to check and variabilize your configuration files",
     long_description="""\
 PyJeb is a lightweight library to check and variabilize your configuration files.
