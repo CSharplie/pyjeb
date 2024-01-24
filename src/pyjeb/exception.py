8cdbd1f3f7d320dffa2160be5329326c847f1a5b
CSharplie <charlie.collier@outlook.fr>
2024-01-24 20:39:35 +0100

    Add detailed exceptions

--- src/pyjeb/exception.py
+++ src/pyjeb/exception.py
@@ -3,5 +3,17 @@
 class InvalidParameterException(Exception):
     """Invalid parameter exception"""
 
+class NotProvidedParameterException(InvalidParameterException):
+    """A parameter is not provided"""
+
+class EmptyParameterException(InvalidParameterException):
+    """A parameter is not provided"""
+
+class InvalidTypeParameterException(InvalidParameterException):
+    """A parameter is not the correct type"""
+
+class InvalidValueParameterException(InvalidParameterException):
+    """A parameter value is correct"""
+
 class CustomFunctionException(Exception):
     """Custom parameter exception"""
